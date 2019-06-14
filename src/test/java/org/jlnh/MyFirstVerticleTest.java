b7e2164e289904e4ea6ac115b1ae826f078795ea
LAPTOP-6GLD5642\João Heckmann <notbestkat@gmail.com>
2019-06-14 19:53:14 -0300

    Adding configuration to the verticle

--- src/test/java/org/jlnh/MyFirstVerticleTest.java
+++ src/test/java/org/jlnh/MyFirstVerticleTest.java
@@ -1,6 +1,8 @@
 package org.jlnh;
 
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
@@ -9,15 +11,30 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 @RunWith(VertxUnitRunner.class)
 public class MyFirstVerticleTest {
 
     private Vertx vertx;
+    private int port = 8081;
 
     @Before
     public void setUp(TestContext context) {
         vertx = Vertx.vertx();
-        vertx.deployVerticle(MyFirstVerticle.class.getName(), context.asyncAssertSuccess());
+
+        try {
+            ServerSocket socket = new ServerSocket(0);
+            port = socket.getLocalPort();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        DeploymentOptions options = new DeploymentOptions() //
+                .setConfig(new JsonObject().put("HTTP_PORT", port));
+        vertx.deployVerticle(MyFirstVerticle.class.getName(), options, context.asyncAssertSuccess());
     }
 
     @After
@@ -29,7 +46,7 @@ public class MyFirstVerticleTest {
     public void testMyApplication(TestContext context) {
         final Async async = context.async();
 
-        vertx.createHttpClient().getNow(8080, "localhost", "/", response -> //
+        vertx.createHttpClient().getNow(port, "localhost", "/", response -> //
                 response.handler(body -> { //
                     context.assertTrue(body.toString().contains("Hey!"));
                     async.complete();
